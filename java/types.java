7670742f62663eab3b66729ab1d2d4a3eab5031c
Eugene Luo <eyluo@andrew.cmu.edu>
2018-12-18 12:03:58 -0500

    adding some Java tutorials

---
+++ java/types.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] Args) {
+        // H3110 w0r1d 2.0 true
+        byte b = 0;
+        short s = 1;
+        int a = 3110;
+        float f = 2.0f;
+        char c = 'r';
+        boolean bool = true;
+        String result = "H" + a + " w" + b + c + s + "d " + f + " " + bool;
+        System.out.println(result);
+    }
+}
